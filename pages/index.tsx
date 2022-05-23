5ce12cf114adc73af36d431f61db79d72728898c
Juano Morello <me@juanomorello.dev>
2022-05-23 19:36:18 -0300

    Initial commit. Added: DaisyUI + TS support

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+const Home = () => {
+  return (
+      <div>
+        <Head>
+          <title>Create Next App</title>
+        </Head>
+        <button className="btn">Hello daisyUI</button>
+      </div>
+  )
+}
+
+export default Home
